272ff34c3dd451d58ac554af83c8dec52d0567af
oellis20 <89800719+oellis20@users.noreply.github.com>
2021-11-30 19:07:11 -0500

    Update Tree.py
    Added code for tree class

---
+++ Tree.py
@@ -0,0 +1,31 @@
+from node import Node
+
+class Tree(Node):
+    def __init__(self, collection):
+        self.parent = None
+        self.children = []
+        Node.__init__(self, collection)
+    
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def get_level(self):
+        level = 0
+        parent = self.parent
+        while parent:
+            level =+ 1
+            parent = parent.parent
+        return level
+    
+    def display(self):
+        spaces = ' ' * self.get_level() * 3
+        if self.parent:
+            prefix = spaces + "|__"
+        else:
+            prefix = ""
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.display()
+        
