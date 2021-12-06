0e1e7c7e20a605f9efe239a6774b5b477d7d3792
oellis20 <89800719+oellis20@users.noreply.github.com>
2021-12-06 00:56:07 -0500

    Update Test.py and Tree.py
    fixed display issues and added recursion of the tree across main function

--- Tree.py
+++ Tree.py
@@ -1,31 +1,22 @@
 from node import Node
 
 class Tree(Node):
-    def __init__(self, collection):
+    def __init__(self, root):
         self.children = []
         self.parent = None
-        Node.__init__(self, collection)
+        Node.__init__(self, root)
     
     def add_child(self, child):
-        Tree(child).parent = self
         self.children.append(child)
-
-    def get_level(self):
-        level = 0
-        parent = self.parent
-        while parent:
-            level =+ 1
-            parent = parent.parent
-        return level
-    
-    def display(self):
-        spaces = ' ' * self.get_level() * 3
-        if self.parent:
-            prefix = spaces + "|__"
-        else:
-            prefix = ""
-        print(prefix + self.data)
-        if self.children:
-            for child in self.children:
-                Tree(child).display()
-        
+        child.parent = self
+                
+    def display(self, level=0):
+        line = '      ' * level
+        if level > 0: 
+            line += "|__"
+        line += self.data["Name"]
+        if self.data["Marriage"]:
+            line += "<-->" + self.data["Marriage"]
+        print(line)
+        for child in self.children:
+            child.display(level + 1) 
--- test.py
+++ test.py
@@ -5,11 +5,17 @@ import json as js
 def main():
     with open('new_monarchs.json', 'r') as file:
         monarchs = js.load(file)
-    monarch = Tree(monarchs["Name"])
-    for child in monarchs["Children"]:
-        monarch.add_child(child["Name"])
-    monarch.display()
+    root_monarch = Tree(monarchs)
+    add_monarchs(root_monarch, monarchs['Children'])
+    root_monarch.display()
 
+def add_monarchs(tree_node, monarcha):
+    if monarcha == None:
+        return
+    for monarch in monarcha:
+        child_monarch = Tree(monarch)
+        tree_node.add_child(child_monarch)
+        add_monarchs(child_monarch, monarch['Children'])
 
 if __name__ == '__main__':
     main()
